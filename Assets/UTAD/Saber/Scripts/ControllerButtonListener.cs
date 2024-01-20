096b3e4b17bc3c6cf736b54484728bfea522d3fe
Anatoli-Remirez <84637222+Anatoli-Remirez@users.noreply.github.com>
2024-01-20 13:32:02 +0100

    OriginBasic

---
+++ Assets/UTAD/Saber/Scripts/ControllerButtonListener.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace Utad.XRInteractionSystem.Input
+{
+	/// <summary>
+	/// Esta clase gestiona la activación de cualquier acción con un controlador
+	/// </summary>
+	public class ControllerButtonListener : MonoBehaviour
+	{
+		[SerializeField] private InputActionReference _actionReference;
+
+		[Space]
+		public UnityEvent _onActionPerformed;
+		public UnityEvent _onActionCancelled;
+
+		private void OnEnable()
+		{
+			_actionReference.action.performed += HandleOnActionPerformed;
+			_actionReference.action.canceled += HandleOnActionCancelled;
+		}
+		
+		private void OnDisable()
+		{
+			_actionReference.action.performed -= HandleOnActionPerformed;
+			_actionReference.action.canceled -= HandleOnActionCancelled;
+		}
+		
+		private void HandleOnActionPerformed(InputAction.CallbackContext obj)
+		{
+			_onActionPerformed.Invoke();	
+		}
+
+		private void HandleOnActionCancelled(InputAction.CallbackContext obj)
+		{
+			_onActionCancelled.Invoke();
+		}
+	}
+}
---
+++ Assets/UTAD/Saber/Scripts/TeleportActivator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Utad.XRInteractionToolkit.Interaction.Input
+{
+	/// <summary>
+	/// Esta clase gestiona la activación del XRController del teleport que tiene como hijo.
+	/// </summary>
+	public class TeleportActivator : MonoBehaviour
+	{
+		[SerializeField] private XRRayInteractor _teleportRayInteractor;
+		private ActionBasedController _teleportActionBasedController;
+
+		private void Awake()
+		{
+			_teleportActionBasedController = _teleportRayInteractor.GetComponent<ActionBasedController>();
+		}
+		
+		public void ActivateTeleport()
+		{
+			TeleportControllerEnabled(true);
+		}
+
+		public void DeactivateTeleport()
+		{
+			Invoke(nameof(WaitAndDeactivate), 0.1f);
+		}
+
+		private void WaitAndDeactivate()
+		{
+			TeleportControllerEnabled(false);
+		}
+
+		private void TeleportControllerEnabled(bool value)
+		{
+			_teleportRayInteractor.enabled = value;
+			_teleportActionBasedController.enableInputActions = value;
+		}
+	}
+}
