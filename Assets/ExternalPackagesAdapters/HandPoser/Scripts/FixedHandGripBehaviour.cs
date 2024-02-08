b60ef631fbaa82d29d7c51e9c80c09ee51e082f2
Manuel J. Barahona <66804505+mjbarahonaga@users.noreply.github.com>
2024-02-08 12:57:13 +0100

    Added XR with direct contact and ray interaction

---
+++ Assets/ExternalPackagesAdapters/HandPoser/Scripts/FixedHandGripBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+namespace Ludus.XRIT.Interactables
+{
+	// <summary>
+	/// Add this to an XRBaseInteractable so it can fix the
+	/// hand over object with pose
+	/// </summary>
+	[RequireComponent(typeof(XRBaseInteractable))]
+	public class FixedHandGripBehaviour : MonoBehaviour
+	{
+
+	}
+}
+
---
+++ Assets/ExternalPackagesAdapters/HandPoser/Scripts/PoseArea.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace LudusXR.ExternalAssetsReferences.HandPoser.Scripts
+{
+	/// <summary>
+	/// Changes hand pose when it enters this collider
+	/// </summary>
+	public class PoseArea : MonoBehaviour
+	{
+		[SerializeField] private Pose _pose;
+
+		[SerializeField] private UnityEvent _onHandEnter;
+		[SerializeField] private UnityEvent _onHandExit;
+		
+		private void OnTriggerEnter(Collider other)
+		{
+			var handWithPose = other.GetComponentInChildren<GameplayHand>();
+			if (handWithPose == null) return;
+			handWithPose.ApplyPose(_pose);
+			_onHandEnter.Invoke();
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			var handWithPose = other.GetComponentInChildren<GameplayHand>();
+			if (handWithPose == null) return;
+			
+			handWithPose.ApplyDefaultPose();
+			_onHandExit.Invoke();
+		}
+	}
+}
